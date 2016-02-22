1d5bec047d96614cad2cfe6bcce52e3a3c3edd8d
nabillo <bnabi@PC-NABILLO>
2016-02-22 00:43:41 +0000

    init

--- src/vigica_edit/view/FXMLMainController.java
+++ src/vigica_edit/view/FXMLMainController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package vigica_edit;
+package vigica_edit.view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,7 +16,7 @@ import javafx.scene.control.Label;
  *
  * @author bnabi
  */
-public class FXMLDocumentController implements Initializable {
+public class FXMLMainController implements Initializable {
     
     @FXML
     private Label label;
